a1338503545c753f2f294b014aea756a794ac46c
yuchenfei <cf.yu@qq.com>
2024-12-18 01:55:37 +0800

    fix(snacks): fix toggle keymap config error

--- lua/plugins/tools/snacks.lua
+++ lua/plugins/tools/snacks.lua
@@ -14,23 +14,21 @@ return {
     { '<leader>S', function() Snacks.scratch.select() end, desc = 'Select Scratch Buffer' },
     { '\\', mode = { 'n', 't' }, function() Snacks.terminal() end, desc = 'Terminal' },
   },
-  opts = function()
-    Snacks.toggle.zen():map('<leader>tz')
-    Snacks.toggle.zoom():map('<leader>tZ'):map('<leader>wm')
-    return {
-      bigfile = { enabled = true },
-      lazygit = { configure = false }, -- use custom config
-      quickfile = { enabled = true },
-      terminal = {
-        win = {
-          keys = {
-            nav_h = { '<C-h>', term_nav('h'), desc = 'Go to Left Window', expr = true, mode = 't' },
-            nav_j = { '<C-j>', term_nav('j'), desc = 'Go to Lower Window', expr = true, mode = 't' },
-            nav_k = { '<C-k>', term_nav('k'), desc = 'Go to Upper Window', expr = true, mode = 't' },
-            nav_l = { '<C-l>', term_nav('l'), desc = 'Go to Right Window', expr = true, mode = 't' },
-          },
+  ---@module 'snacks'
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    lazygit = { configure = false }, -- use custom config
+    quickfile = { enabled = true },
+    terminal = {
+      win = {
+        keys = {
+          nav_h = { '<C-h>', term_nav('h'), desc = 'Go to Left Window', expr = true, mode = 't' },
+          nav_j = { '<C-j>', term_nav('j'), desc = 'Go to Lower Window', expr = true, mode = 't' },
+          nav_k = { '<C-k>', term_nav('k'), desc = 'Go to Upper Window', expr = true, mode = 't' },
+          nav_l = { '<C-l>', term_nav('l'), desc = 'Go to Right Window', expr = true, mode = 't' },
         },
       },
-    }
-  end,
+    },
+  },
 }
--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -2,10 +2,13 @@
 
 return {
   'folke/snacks.nvim',
+  ---@module 'snacks'
+  ---@type snacks.Config
   opts = {
     indent = { enabled = true },
     notifier = { enabled = true },
     scope = { enabled = true },
+    scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
     dashboard = { enabled = true },
