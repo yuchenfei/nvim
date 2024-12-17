98bd39c149bd311251af5e563b8dd73d228cbad3
yuchenfei <cf.yu@qq.com>
2024-12-17 11:33:07 +0800

    feat(blink): optimize config

--- lua/config/code/blink.lua
+++ lua/config/code/blink.lua
@@ -15,31 +15,44 @@ return function()
     completion = {
       menu = {
         border = 'rounded',
+        draw = {
+          treesitter = { 'lsp' },
+        },
       },
       documentation = {
         auto_show = true,
+        auto_show_delay_ms = 200,
         window = {
           border = 'rounded',
         },
       },
+      ghost_text = {
+        enabled = true,
+      },
     },
     signature = { enabled = true },
     sources = {
-      -- default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'copilot' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'copilot' },
       completion = {
         enabled_providers = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'copilot' },
       },
       providers = {
         -- dont show LuaLS require statements when lazydev has items
         lsp = { fallback_for = { 'lazydev' } },
-        lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink' },
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          score_offset = 100,
+        },
         copilot = {
           name = 'copilot',
           module = 'blink-cmp-copilot',
+          async = true,
+          score_offset = 100,
         },
       },
       -- optionally disable cmdline completions
-      -- cmdline = {},
+      cmdline = {},
     },
     appearance = {
       nerd_font_variant = 'mono', -- mono or normal
