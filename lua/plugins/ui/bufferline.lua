0cac2512c95f143065eac225c13260c65e494f41
yuchenfei <cf.yu@qq.com>
2024-12-18 02:41:27 +0800

    feat(persistence): add persistence.nvim

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -37,4 +37,13 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require('bufferline').setup(opts)
+    -- Fix bufferline when restoring a session
+    vim.api.nvim_create_autocmd({ 'BufAdd', 'BufDelete' }, {
+      callback = function()
+        vim.schedule(function() pcall(nvim_bufferline) end)
+      end,
+    })
+  end,
 }
